e05a0978dbe8bf53b88d0808a37dd9f8726f8198
Julia Bian <45663604+jbian92@users.noreply.github.com>
2021-08-22 02:27:15 +0000

    added styling

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-// This function, caesar_cipher(), is the function we're writing together. It's your job to fill it in!
-
-// encrypts plain text into ciphertext and that decrypts ciphertext into plain text
+// Encrypts and Decrypts
 // positive key = encrypts
 // negative key = decrypts
 function caesar_cipher(string, key) {
-    
   var output = "";
+
+  if ((key > 26) || (key < -26)) {
+    key = key % 26;
+  }
 
   for (var i = 0; i < string.length; i++) {
     var unicode = string.charCodeAt(i);
-    // Uppercase
+
+    // Uppercase letter
     if ((unicode >= 65) && (unicode <= 90)) {
-      // shift any uppercase letters down to the 0-25 range before we can use the % operator, and then shift it back to the 65-90 range to convert the resulting Unicode value back into a string
+      // Shift any uppercase letters down to the 0-25 range before using the % operator
+      // Then shift it back to the 65-90 range to convert the resulting Unicode value back into a string
       var res_unicode = ((26 + unicode - 65 + key) % 26) + 65;
       output += String.fromCharCode(res_unicode);
     }
-    // Lowercase
+    // Lowercase letter
     else if ((unicode >= 97) && (unicode <= 122)) {
       var res_unicode = ((26 + unicode - 97 + key) % 26) + 97;
       output += String.fromCharCode(res_unicode);
@@ -27,14 +30,7 @@ function caesar_cipher(string, key) {
   }
 
   return output;
-
 }
-
-
-// Everything below this comment is part of your starter code. It helps our program run but is not part of the concept we're learning. Feel free to play around to understand it!
-// ----------------------------
-
-// This code uses the function we've written, caesar_cipher() and manipulates the text in the HTML textboxes to pass as an argument to our function. It also displays the ciphertext and plain text that is generated.
 
 function encrypt() {
   console.log("Encrypted!");
@@ -46,9 +42,6 @@ function decrypt() {
   document.getElementById("decrypt_output").innerText = caesar_cipher(document.getElementById("ciphertext").value, -parseInt(document.getElementById("decrypt_key").value));
 }
 
-
-// This code makes sure that we call our functions upon clicking the buttons.
-
+// Makes sure that the encrypt and decrypt functions are called upon clicking the corresponding buttons
 document.getElementById("encrypt").addEventListener("click", function() {encrypt()});
-
 document.getElementById("decrypt").addEventListener("click", function() {decrypt()});
